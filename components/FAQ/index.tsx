9de8a0304ba714f4d1c1f8cab74f3d8206249e2f
Trevor Mangrum <25257772+Vaeldon@users.noreply.github.com>
2020-10-15 21:41:48 -0500

    Implements FAQ page and FAQ component (#18)
    Co-authored-by: Trevor Mangrum <mangrumtrevor@gmail.com>

---
+++ components/FAQ/index.tsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import styles from "./faq.module.scss";
+
+interface Props {
+  question: string; //Pass in question as a prop
+  children: React.ReactNode; //Children would be the answer to the question
+}
+
+const FAQ: React.FC<Props> = ({ question, children }) => {
+  const [collapsed, setCollapsed] = useState(true); //Clicking the button will adjust the maximum height of the question, which makes it slide open
+  return (
+    <section>
+      <button
+        className={`${styles["accordion"]} ${
+          collapsed ? "" : styles["active"]
+        }`}
+        onClick={() => setCollapsed(!collapsed)}
+      >
+        {question}
+      </button>
+      <div className={collapsed ? styles["collapsed"] : styles["visible"]}>
+        <p>{children}</p>
+      </div>
+    </section>
+  );
+};
+export default FAQ;
