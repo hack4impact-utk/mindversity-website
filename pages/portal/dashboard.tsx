6349f38bbe1b0ca03f0274f31b14460fb539efc7
Ray Luberto <rluberto15@gmail.com>
2020-10-18 11:58:11 -0400

    Admin Portal Dashboard (#20)
    * admin portal header draft
    
    * started dashboard buttons
    
    * remove background image

--- pages/portal/dashboard.tsx
+++ pages/portal/dashboard.tsx
@@ -13,13 +13,62 @@ const Dashboard: NextPage = () => {
 
       <Header />
 
+      <h1>Welcome to MindVersity</h1>
+      <h2>A national peer network.</h2>
+
+      <div className="dashChipParent">
+        <h2>What yould you like to do?</h2>
+        <a className="dashChip" href="chapters">
+          Manage Chapters
+        </a>
+        <a className="dashChip" href="resources">
+          Edit Resources
+        </a>
+        <a className="dashChip" href="journal">
+          Update Journal
+        </a>
+      </div>
+
       <style jsx>{`
-        .container {
-          min-height: 100vh;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
+        .container{
+          padding-top: 50px;
+          text-align: center;
+        }
+
+        .dashChipParent{
+          width: auto;
+          height: auto;
+          position: relative;
+          display: block;
+          margin-top: 50px;
+          padding: 40px;
+        }
+
+        .dashChipParent h2{
+          margin-bottom: 40px;
+        }
+
+        .dashChip{
+          width: auto;
+          height: auto;
+          position: relative;
+          display: inline-block;
+          padding: 20px 40px;
+          font-size: 20px;
+          border-radius: 500px;
+          border: 3px solid #2AB7CA;
+          box-shadow: 0px 5px 5px #dedede;
+          background-color: #FAFAFA;
+          color: black;
+          text-decoration: none;
+          margin: 20px;
+          transition background 0.5s ease;
+        }
+
+        .dashChip:hover{
+          background-color: #2AB7CA;
+          color: white;
+          cursor: pointer;
         }
       `}</style>
 
