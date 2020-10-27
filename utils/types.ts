e8555a56817bf8389203a1f8c92e63b1dd4306ce
Kemal Fidan <kemalfidan3@gmail.com>
2020-10-27 15:36:17 -0400

    finalized chapter types

--- utils/types.ts
+++ utils/types.ts
@@ -1,11 +1,19 @@
-import { Mongoose } from "mongoose";
 import { ObjectID } from "mongodb";
 
+export interface Officer {
+    id?: ObjectID;
+    name?: string;
+    picture?: string;
+    role?: string;
+}
 
 // Keep in sync with the backend schema
 export interface Chapter {
-    id?: ObjectID
+    id?: ObjectID;
     name?: string;
     region?: string;
-    memberCount?: number
+    description?: string;
+    universityLogo?: string;
+    campusPic?: string;
+    officers?: Officer[];
 }
