41e68306fb3511b4ec8088316e2f125664040390
Ray Luberto <rluberto15@gmail.com>
2020-11-07 21:00:09 +0000

    update partners to match new ui

--- components/Partners/index.tsx
+++ components/Partners/index.tsx
@@ -1,22 +1,27 @@
 import React from "react";
-import styles from "./partners.module.css";
+import styles from "./partners.module.scss";
 
 const Partners: React.FC = () => {
   return (
-    <section className={styles["container"]}>
-      <div className={styles["partner"]}>
+    <section className={styles.container}>
+      <div className={styles.partnerTitle}>
+        <img src="/logo.svg" alt="MindVersity Logo"></img>
+        <h2>Partners</h2>
+      </div>
+      <br></br>
+      <div className={styles.partner}>
         <img
           src="https://www.stevefund.org/wp-content/uploads/2019/03/The-Steve-Fund-logo-vertical-color.png"
           alt="Partner logo"
         ></img>
       </div>
-      <div className={styles["partner"]}>
+      <div className={styles.partner}>
         <img
           src="https://www.artwithimpact.org/wp-content/uploads/awi_logo_020711_0-1.png"
           alt="Partner logo"
         ></img>
       </div>
-      <div className={styles["partner"]}>
+      <div className={styles.partner}>
         <img
           src="https://pbs.twimg.com/profile_images/1102970449472950273/xGIOGqeH_400x400.jpg"
           alt="Partner logo"
