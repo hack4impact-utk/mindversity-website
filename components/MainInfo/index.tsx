1d18a5ba4577e2353ef01664c6f8f1b5b3262ae4
Ray Luberto <rluberto15@gmail.com>
2020-11-08 21:41:14 +0000

    update main info component to match new UI

--- components/MainInfo/index.tsx
+++ components/MainInfo/index.tsx
@@ -1,42 +1,45 @@
 import React from "react";
-import community from "./community.svg";
-import characters from "./characters.svg";
+import classNames from 'classnames'
+import heroBackground from "./herobackground.png";
+import aboutBackground from "./characters.svg";
 import styles from "./maininfo.module.scss";
 
 const MainInfo: React.FC = () => (
-  <section className={styles["container"]}>
-    <div className={styles["wrapper"]}>
-      <header className={styles["opening-text"]}>
-        <p>A national peer network.</p>
-        <p>A call to action.</p>
-        <p>An initiative.</p>
-        <p>A change.</p>
-      </header>
-      <img
-        src={community}
-        alt="Three characters."
-        className={styles["image"]}
-      />
+  <section className={styles.container}>
+     
+    <div className={styles.hero}>
+      <div className={styles.heroOverlay}>
+        <div className={styles.heroText}>
+          <h1>Our Voices. Our Stories. Our Minds.</h1>
+          <h2>
+            A Global Effort to Better BIPOC Student Mental Health Care.
+          </h2>
+        </div>
+      </div>
+      <img src={heroBackground} alt="MindVersity strives to bridge the gaps between access to mental health resources and students of color on college campuses nationwide."/>
     </div>
 
-    <div className={styles["wrapper"]}>
-      <img
-        src={characters}
-        alt="Characters hanging out."
-        className={styles["image"]}
-      />
-      <div className={styles["description"]}>
-        <h2>Welcome to MindVersity.</h2>
-        <p>
-          Since 2020, MindVersity has helped create supportive communities for
-          individuals who are overcoming difficulties. We understand how helpful
-          it is for people going through life issues to find a space where they
-          can build connections with others who understand exactly what
-          they&apos;re experiencing. Feel free to reach out for additional
-          information.
-        </p>
+    <div className={styles.aboutParent}>
+
+      <div className={styles.aboutCol}>
+        <div className={styles.aboutText}>
+          <h2>Mental Health For All</h2>
+          <p>
+            MindVersity strives to bridge the gaps between access to mental health resources and students of color on college campuses nationwide. We aim to do this by creating peer support networks across campuses, reducing cultural stigma through education, and generating a platform where resources are readily made accessible.
+          </p>
+        </div>
       </div>
+
+      <div className={classNames(styles.aboutCol, styles.aboutBackgroundCol)}>
+        <img src={aboutBackground} alt="MindVersity is a place for students of color to get easire access to mental health resources."></img>
+      </div>
+
     </div>
+
+    <div className={styles.quoteParent}>
+      "People of color...feel the stigma [of mental health] more keenly. In a race-conscious society, some don't want to be perceived as having yet another deficit."  
+    </div>
+
   </section>
 );
 
