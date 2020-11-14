aac5863e498d4ef9e14d17c6dc74089668453387
Jovan Yoshioka <54458982+jovanyoshioka@users.noreply.github.com>
2020-11-13 19:29:12 -0500

    Updated footer component to match figma. (#37)

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,11 +1,5 @@
 import React from 'react';
 import styles from "./footer.module.scss";
-import fb from '../../public/icons/facebook.png'
-import spotify from '../../public/icons/spotify.png'
-import tiktok from '../../public/icons/tiktok.png'
-import insta from '../../public/icons/instagram.png'
-import twitter from '../../public/icons/twitter.png'
-import logo from '../../public/mindversity-logo.png'
 
 export default function Footer() {
     return (
@@ -13,35 +7,36 @@ export default function Footer() {
             <div className={styles.wrapper}>
                 <div className={styles.row}>
                     <div className={styles.column}>
-                      <img src={logo} className={styles.logo}/>
+                        <p className={styles.centeredTitle}>We'd Love To Hear From You</p>
+                        <span>
+                            <a className={styles.link} href="/">Contact Us</a>
+                        </span>
                     </div>
                     <div className={styles.column}>
-                        <p className={styles.title}>About Us</p>
-                        <a className={styles.link} href="/">Mission</a>
-                        <a className={styles.link} href="/">Team</a>
-                        <a className={styles.link} href="/">Chapters</a>
+                        <p className={styles.title}>General</p>
+                        <span>
+                            <a className={styles.link} href="/journal">Journal</a>
+                            <a className={styles.link} href="/chapters">Chapters</a>
+                            <a className={styles.link} href="/resources">Resources</a>
+                            <a className={styles.link} href="/faq">FAQ</a>
+                        </span>
                     </div>
                     <div className={styles.column}>
-                        <p className={styles.title}>Peer Network</p>
-                        <a className={styles.link} href="/">Become an Advocate</a>
-                        <a className={styles.link} href="/">Get Support</a>
-                        <a className={styles.link} href="/">Forum</a>
-                        <a className={styles.link} href="/">Design</a>
+                        <p className={styles.title}>Get Involved</p>
+                        <span>
+                            <a className={styles.link} href="/">Start Chapter</a>
+                            <a className={styles.link} href="/">Students</a>
+                            <a className={styles.link} href="/">Contact Us</a>
+                        </span>
                     </div>
-                    <div className={styles.column}>
-                        <p className={styles.title}>Resources</p>
-                        <a className={styles.link} href="/">MindVersity Blogs</a>
-                        <a className={styles.link} href="/">Community Blogs</a>
-                        <a className={styles.link} href="/">Write a Blog</a>
-                    </div>
-                    <div className={styles.column}>
-                        <p className={styles.title}>Social Media</p>
-                        <a className={styles.link} href="https://www.facebook.com/MindVersed/"><img className={styles.icon} src={fb} />Facebook</a>
-                        <a className={styles.link} href="https://twitter.com/MindVersity/media"><img className={styles.icon} src={twitter} />Twitter</a>
-                        <a className={styles.link} href="https://www.instagram.com/mindversityorg"><img className={styles.icon} src={insta} />Instagram</a>
-                        <a className={styles.link} href="https://open.spotify.com/show/1VDhTv2Y0K180XMf9NY23W?si=HhIdhDlHRU2ZOiiH4WFF9Q"><img className={styles.icon} src={spotify} />Spotify</a>
-                        <a className={styles.link} href="/@mindversityorg"><img className={styles.icon} src={tiktok} />Tiktok</a>
-                    </div>
+                </div>
+            </div>
+            <div className={styles.row}>
+                <div className={styles.column}>
+                    <p>A Hack4Impact Chapter</p>
+                </div>
+                <div className={styles.column}>
+                    <p className={styles.logo}>MindVersity</p>
                 </div>
             </div>
         </div>
