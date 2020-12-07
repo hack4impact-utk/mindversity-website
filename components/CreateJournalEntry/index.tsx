dab79ab23623271a460b4ec67a6a8d0c9a5669d5
Trevor Mangrum <mangrumtrevor@gmail.com>
2020-12-07 17:59:37 -0600

    Implements Quill editor and finalizes styles on frontend

--- components/CreateJournalEntry/index.tsx
+++ components/CreateJournalEntry/index.tsx
@@ -3,6 +3,8 @@ import Link from "next/link";
 import styles from "./create.module.scss";
 import { FaArrowLeft } from "react-icons/fa";
 import {BiImageAdd} from "react-icons/bi";
+import dynamic from "next/dynamic"
+const ReactQuill = dynamic(import('react-quill'), { ssr: false});
 const CreateJournalEntry: React.FC = () => {
     return(
         <section className={styles['container']}>
@@ -20,14 +22,22 @@ const CreateJournalEntry: React.FC = () => {
                         </div>
                         <input type="file" name="image" className={styles['image-select']}/>
                     </div>
-                    <input type="text" name="title" placeholder="Title"/>
-                    <input type="text" name="description" placeholder="Description" />
-
-                    <select name="category" className={styles['category']}>
-                        <option value="">Please select a category</option>
-                        <option value="vent-place">Vent Place</option>
-                        <option value="creative-space">Creative Space</option>
-                    </select>
+                    <div className={styles['text-container']}>
+                        <label htmlFor="title">Title</label>
+                        <input type="text" name="title" placeholder="Title"/>
+                        <label htmlFor="description">Description</label>
+                        <input type="text" name="description" placeholder="Description" />
+                        <label htmlFor="body">Body Paragraph</label>
+                        <ReactQuill theme="snow" className={styles['editor']} placeholder="Enter your journal body here."/>
+                        <div className={styles['select-container']}>
+                            <span>Publish to: </span>
+                            <select name="category" className={styles['category']}>
+                                <option value="">Please select a category</option>
+                                <option value="vent-place">Vent Place</option>
+                                <option value="creative-space">Creative Space</option>
+                            </select>
+                        </div>
+                    </div>
                     <button type="submit" className={styles['submit']}>Publish</button>
                 </form>
             </div>
--- pages/journal/create.tsx
+++ pages/journal/create.tsx
@@ -6,6 +6,7 @@ import CreateJournalEntry from "components/CreateJournalEntry";
 const Create: NextPage = () => {
     return(
         <main>
+            <link rel="stylesheet" href="//cdn.quilljs.com/1.2.6/quill.snow.css" />
             <Header/>
             <CreateJournalEntry/>
             <Footer/>
