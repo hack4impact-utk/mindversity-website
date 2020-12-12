752d69263c8775a08bd851ece30e4910821880fd
Trevor Mangrum <mangrumtrevor@gmail.com>
2020-12-12 16:02:44 -0600

    Renames /api/journal/get to /api/journal/getByReviewStatus and fixes an issue with boolean variable

--- pages/api/journal/getByReviewStatus.ts
+++ pages/api/journal/getByReviewStatus.ts
@@ -1,12 +1,13 @@
 import { NextApiResponse, NextApiRequest } from "next";
 import {getJournalEntriesByReviewStatus} from "server/actions/Contentful";
-//This API route takes in a query from the url. By default, the route will return all entries that have not been reviewed. To get reviewed entries, use the following route: /api/journal/get?reviewed=true
+//This API route takes in a query from the url. By default, the route will return all entries that have not been reviewed. To get reviewed entries, use the following route: /api/journal/getByReviewStatus?reviewed=true
 export default async function handler(req: NextApiRequest, res: NextApiResponse){
 
     if(req.method == "GET"){
         let reviewed = false;
+        //Req.query.reviewed is a string, but getJournalEntriesByReviewStatus expects a boolean. To make it one, we check if req.query.reviewed === "true". If it is, then we have a boolean review status with true. If not, the boolean status remains false.
         if(req.query.reviewed){
-           reviewed = (req.query.reviewed === "true") || (req.query.reviewed === "false");
+           reviewed = (req.query.reviewed === "true");
         }
         const entries = await getJournalEntriesByReviewStatus(reviewed);
         if(entries != {}){
