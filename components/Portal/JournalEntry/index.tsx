9f3b6111eb6248e2e6761a2df9f37fd812bf44a9
Trevor Mangrum <mangrumtrevor@gmail.com>
2020-12-14 14:46:58 -0600

    Fixes a comment within the journal entry component

--- components/Portal/JournalEntry/index.tsx
+++ components/Portal/JournalEntry/index.tsx
@@ -2,7 +2,7 @@ import styles from "./journalentry.module.scss";
 import {EntryProp} from "contentful-management/dist/typings/entities/entry";
 interface Props{
     entry: EntryProp;
-    mode: string; //Because Edit Resources and Update Journal require a similar component, this prop allows us to swap out things needed in each inside this component. This keeps us from having to make two components that are basically the same thing. There's two modes: delete and review.
+    mode: string; //Because Delete Journal and Update Journal require a similar component, this prop allows us to swap out things needed in each inside this component. This keeps us from having to make two components that are basically the same thing. There's two modes: delete and review.
 }
 const JournalEntry: React.FC<Props> = ({entry, mode}) => {
     return(
