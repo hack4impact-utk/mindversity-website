e0ab009bcae4ec7f9266cea69693fdc29885adea
Kemal Fidan <kemalfidan3@gmail.com>
2020-12-16 19:08:30 -0600

    Finished updateChapter(), haven't tested yet

--- pages/api/chapter/addChapter.ts
+++ pages/api/chapter/addChapter.ts
@@ -3,7 +3,9 @@ import { uploadImage } from "server/actions/Contentful";
 import { addChapter } from "server/actions/Chapter";
 import formidable from "formidable";
 import { Chapter } from "utils/types";
-//To get formidable to work, bodyParser has to be turned off. Otherwise, the parse request will never end.
+
+// To get formidable to work, bodyParser has to be turned off. 
+// Otherwise, the parse request will never end.
 export const config = {
     api: {
         bodyParser: false,
@@ -12,13 +14,12 @@ export const config = {
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     const form = new formidable.IncomingForm();
-    form.parse(req, async (err: any, fields: formidable.Fields, files: formidable.Files) => {
+    form.parse(req, async (err: any, fields: formidable.Fields, files: formidable.Files) => { 
         //Fields is used for everything other than files, so all this data can be passed in directly to the chapter type.
-        const chapterInfo: Chapter = fields;
-
+        let chapterInfo: Chapter = fields;
+        
         //Since we use names in the url, names need to be parsed and have all whitespace converted to underscores
-        if (chapterInfo.name)
-            chapterInfo.name = chapterInfo.name.replace(/ /g, "_");
+        chapterInfo.name = chapterInfo?.name?.replace(/ /g, "_");
 
         //Since these two don't rely on each other to be uploaded, doing this allows them to be done simultaneously, and should be more efficient
         [
