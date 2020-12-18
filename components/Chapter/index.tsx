ec0b8dd35481b83f4345333fbce20229f110c324
Trevor <mangrumtrevor@gmail.com>
2020-12-18 12:40:45 -0600

    Fixes linter issues in all Components except for HomeChapters, Officers, OfficerCarousel, and CreateJournalEntry

--- components/Chapter/index.tsx
+++ components/Chapter/index.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { Chapter } from 'utils/types'
+import React from "react";
+import { Chapter } from "utils/types";
 
 interface Props {
-  chapter: Chapter;
+    chapter: Chapter;
 }
 
 const ChapterComp: React.FC<Props> = ({ chapter }) => {
-  return (
-    <div>
-      <p> chapter name: { chapter?.name } </p>
-      <p> chapter region: { chapter.region } </p>
-      <p> the whole chapter object: { JSON.stringify(chapter) } </p>
-    </div>
-  );
+    return (
+        <div>
+            <p> chapter name: {chapter?.name} </p>
+            <p> chapter region: {chapter.region} </p>
+            <p> the whole chapter object: {JSON.stringify(chapter)} </p>
+        </div>
+    );
 };
 
 export default ChapterComp;
--- components/HomeChapters/index.tsx
+++ components/HomeChapters/index.tsx
@@ -1,50 +1,52 @@
-import React, { useState } from "react";
+import React from "react";
 import style from "./style.module.scss";
 import { getRegion } from "utils/helpers";
 
 const HomeChapters: React.FC = () => {
-  const [region, setRegion] = React.useState("unknown");
+    const [region, setRegion] = React.useState("unknown");
 
-  React.useEffect(() => {
-    if (navigator) {
-      navigator.geolocation.getCurrentPosition((pos: Position) => {
-        const [lat, long] = [pos.coords.latitude, pos.coords.longitude];
-        setRegion(getRegion(lat, long));
-      });
-    }
-  });
+    React.useEffect(() => {
+        if (navigator) {
+            navigator.geolocation.getCurrentPosition((pos: Position) => {
+                const [lat, long] = [pos.coords.latitude, pos.coords.longitude];
+                setRegion(getRegion(lat, long));
+            });
+        }
+    });
 
-  return (
-    <div className={style.parentContainer}>
-      <h2 className={style.title}>Chapters In Your Region</h2>
-      <div className={style.cardWrapper}>
-        <a href="chapters" className={style.card}>
-          <div className={style.cardOverlay}>
-            <div className={style.cardText}>University of Tennessee</div>
-          </div>
-          <img
-            src="/utk-placeholder.jpg"
-            className={style.cardImage}
-            alt="Chapter name here"
-          ></img>
-        </a>
+    return (
+        <div className={style.parentContainer}>
+            <h2 className={style.title}>Chapters In Your Region</h2>
+            <div className={style.cardWrapper}>
+                <a href="/chapters" className={style.card}>
+                    <div className={style.cardOverlay}>
+                        <div className={style.cardText}>
+                            University of Tennessee
+                        </div>
+                    </div>
+                    <img
+                        src="/utk-placeholder.jpg"
+                        className={style.cardImage}
+                        alt="Chapter name here"
+                    ></img>
+                </a>
 
-        <a href="chapters" className={style.card}>
-          <div className={style.cardOverlay}>
-            <div className={style.cardText}>Rutgers</div>
-          </div>
-          <img
-            src="/rutgers-placeholder.jpg"
-            className={style.cardImage}
-            alt="Chapter name here"
-          ></img>
-        </a>
-      </div>
-      <a href="chapters" className={style.exploreBtn}>
-        Explore All
-      </a>
-    </div>
-  );
+                <a href="/chapters" className={style.card}>
+                    <div className={style.cardOverlay}>
+                        <div className={style.cardText}>Rutgers</div>
+                    </div>
+                    <img
+                        src="/rutgers-placeholder.jpg"
+                        className={style.cardImage}
+                        alt="Chapter name here"
+                    ></img>
+                </a>
+            </div>
+            <a href="chapters" className={style.exploreBtn}>
+                Explore All
+            </a>
+        </div>
+    );
 };
 
 export default HomeChapters;
