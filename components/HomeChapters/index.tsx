e8de61dbcde3db49691012939f1f24549cfab543
Trevor <mangrumtrevor@gmail.com>
2020-12-19 10:10:28 -0600

    Rebase

--- components/HomeChapters/index.tsx
+++ components/HomeChapters/index.tsx
@@ -3,7 +3,6 @@ import style from "./style.module.scss";
 import { Chapter } from "utils/types";
 import { getRegion } from "utils/helpers";
 
-<<<<<<< HEAD
 interface Props {
   chapters: Chapter[];
 }
@@ -100,53 +99,6 @@ const HomeChapters: React.FC<Props> = ({ chapters }) => {
       </a>
     </div>
   );
-=======
-const HomeChapters: React.FC = () => {
-    const [region, setRegion] = React.useState("unknown");
-
-    React.useEffect(() => {
-        if (navigator) {
-            navigator.geolocation.getCurrentPosition((pos: Position) => {
-                const [lat, long] = [pos.coords.latitude, pos.coords.longitude];
-                setRegion(getRegion(lat, long));
-            });
-        }
-    });
-
-    return (
-        <div className={style.parentContainer}>
-            <h2 className={style.title}>Chapters In Your Region</h2>
-            <div className={style.cardWrapper}>
-                <a href="/chapters" className={style.card}>
-                    <div className={style.cardOverlay}>
-                        <div className={style.cardText}>
-                            University of Tennessee
-                        </div>
-                    </div>
-                    <img
-                        src="/utk-placeholder.jpg"
-                        className={style.cardImage}
-                        alt="Chapter name here"
-                    ></img>
-                </a>
-
-                <a href="/chapters" className={style.card}>
-                    <div className={style.cardOverlay}>
-                        <div className={style.cardText}>Rutgers</div>
-                    </div>
-                    <img
-                        src="/rutgers-placeholder.jpg"
-                        className={style.cardImage}
-                        alt="Chapter name here"
-                    ></img>
-                </a>
-            </div>
-            <a href="chapters" className={style.exploreBtn}>
-                Explore All
-            </a>
-        </div>
-    );
->>>>>>> ec0b8dd (Fixes linter issues in all Components except for HomeChapters, Officers, OfficerCarousel, and CreateJournalEntry)
 };
 
 export default HomeChapters;
