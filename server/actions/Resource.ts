f73a1f69c8fb9daebeb9c97dc993b6f8a21c24e3
Kemal Fidan <kemalfidan3@gmail.com>
2020-12-18 18:42:41 -0600

    finished resources

--- server/actions/Resource.ts
+++ server/actions/Resource.ts
@@ -6,35 +6,21 @@ import { Resource } from "utils/types";
  * Adds the given resource to the database
  * @param resource New Resource to add to DB
  */
-export const addResource = async function (
-    resource: Resource
-): Promise<boolean> {
+export const addResource = async function (resource: Resource): Promise<void> {
     await mongoDB();
-
-    if (await ResourceModel.create(resource)) {
-        return true;
-    }
-
-    return false;
+    await ResourceModel.create(resource);
 };
 
 /**
  * Retrieves the resources that match the given resource
  * @param resource The resource info to search for
  * @returns An array containing the matching resources
- * @throws Error if resource does not exist
  */
-export const getResource = async function (
-    resource: Resource
-): Promise<Resource[]> {
+export const getResources = async function (resource: Resource): Promise<Resource[]> {
     await mongoDB();
 
     if (!resource) resource = {};
-
     const resourcesList = await ResourceModel.find(resource).lean();
-
-    if (!resourcesList) throw new Error("Resource does not exist");
-
     return resourcesList;
 };
 
@@ -43,24 +29,13 @@ export const getResource = async function (
  * @param resource The resource to update (must include resource ID)
  * @throws Error if document does not exist and if ID is not provided
  */
-export const updateResource = async function (
-    resource: Resource
-): Promise<boolean> {
+export const updateResource = async function (resource: Resource): Promise<void> {
     if (!resource._id) throw new Error("Id must be provided for update");
 
     await mongoDB();
 
     const oldResource = { _id: resource._id };
-
-    if (
-        await ResourceModel.findOneAndUpdate(oldResource, resource, {
-            upsert: false,
-        })
-    ) {
-        return true;
-    }
-
-    return false;
+    await ResourceModel.findOneAndUpdate(oldResource, resource, {upsert: false});
 };
 
 /**
@@ -68,25 +43,11 @@ export const updateResource = async function (
  * @param resource The resource to delete
  * @throws Error if resource is not found
  */
-export const deleteResource = async function (
-    resource: Resource
-): Promise<boolean> {
+export const deleteResource = async function (resource: Resource): Promise<void> {
     await mongoDB();
-    let deleted = false;
 
-    if (resource._id) {
-        await ResourceModel.findByIdAndDelete(resource._id).then(
-            (resource: Resource) => {
-                if (resource) deleted = true;
-            }
-        );
-    } else {
-        await ResourceModel.findOneAndDelete(resource).then(
-            (resource: Resource) => {
-                if (resource) deleted = true;
-            }
-        );
-    }
-
-    return deleted;
+    if (resource._id) 
+        await ResourceModel.findByIdAndDelete(resource._id);
+    else 
+        await ResourceModel.findOneAndDelete(resource);
 };
