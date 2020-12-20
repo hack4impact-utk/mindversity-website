ef1b7e95933dffb44ec5d6513a39492c2be86f70
VishalAiely <vaiely@vols.utk.edu>
2020-12-20 17:38:50 -0600

    Password Protected portal pages Also linked portal journal entries to journal view

--- pages/portal/chapters/[name].tsx
+++ pages/portal/chapters/[name].tsx
@@ -1,11 +1,36 @@
-import { NextPage } from "next";
+import { NextPage, NextPageContext } from "next";
+import Router from "next/router";
 
 const nameChapterPage: NextPage = () => {
-    return(
-        <div>
+    return <div></div>;
+};
 
-        </div>
-    )
+export async function getServerSideProps(context: NextPageContext) {
+    const cookie = context.req?.headers.cookie;
+
+    //Since this is client side only absolute URLs are supported
+    //TODO: need to change url off of localhost in production
+    const resp = await fetch("http://localhost:3000/api/admin/validateLogin", {
+        headers: {
+            cookie: cookie!,
+        },
+    });
+
+    if (resp.status === 401 && !context.req) {
+        void Router.replace("/portal/login");
+        return { props: {} };
+    }
+
+    if (resp.status === 401 && context.req) {
+        context.res?.writeHead(302, {
+            //TODO: same here
+            Location: "http://localhost:3000/",
+        });
+        context.res?.end();
+        return { props: {} };
+    }
+
+    return { props: {} };
 }
 
 export default nameChapterPage;
--- utils/urls.ts
+++ utils/urls.ts
@@ -1,12 +1,16 @@
 const prod = process.env.NODE_ENV === "production";
 
 export default {
-    baseUrl: prod ? process.env.PROD_URL : "http://localhost:3000",
+    baseUrl: prod ? (process.env.PROD_URL as string) : "http://localhost:3000",
     dbUrl: process.env.MONGO_DB ?? "mongodb://localhost:27017",
     pages: {
         index: "/",
         passwordReset: "portal/password-reset",
         newPassword: "portal/new-password",
+        journal: {
+            index: "/journal",
+            create: "/journal/create",
+        },
     },
     api: {
         admin: {
