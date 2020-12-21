d37664ff77e390c1a431a9f19734e2417050d9d2
VishalAiely <vaiely@vols.utk.edu>
2020-12-20 21:53:19 -0600

    Added chapter specific resources to chapter pages simply displays and does not account for how many

--- pages/chapters/[name].tsx
+++ pages/chapters/[name].tsx
@@ -3,237 +3,275 @@ import Footer from "components/Footer";
 import OfficerCarouselComp from "components/OfficerCarousel";
 import { getChapters } from "server/actions/Chapter";
 import { getOfficers } from "server/actions/Officer";
-import { Chapter, Officer } from 'utils/types';
-import { GetStaticPropsContext, NextPage, NextPageContext } from 'next';
+import { getResources } from "server/actions/Resource";
+import { Chapter, Officer, Resource } from "utils/types";
+import { GetStaticPropsContext, NextPage, NextPageContext } from "next";
 import { FaMapMarkerAlt } from "react-icons/fa";
-import errors from 'utils/errors';
+import errors from "utils/errors";
 import Head from "next/head";
 
 // When routing here we have a chapter name we can get from the url name
 // We can get that param with useRouter(), but its also given in the context
 
 interface Props {
-  chapter: Chapter,
-  officers: Officer[],
+    chapter: Chapter;
+    officers: Officer[];
+    resources: Resource[];
 }
 
-const ChapterPage: NextPage<Props> = ({ chapter, officers }) => {
+const ChapterPage: NextPage<Props> = ({ chapter, officers, resources }) => {
+    //Replace any underscores in the chapter name with spaces
+    const cleanName = chapter.name?.replace(/_/g, " ");
+    //Make the first letter of the region name capital
 
-  //Replace any underscores in the chapter name with spaces
-  var cleanName = chapter.name?.replace(/_/g, " ");
-  //Make the first letter of the region name capital
-
-  var heroStyle;
-  if(chapter.campusPic?.url){ //Add the background image to the style if the url exists in the db
-    heroStyle = {
-      backgroundImage: 'url(' + chapter.campusPic.url + ')'
+    let heroStyle;
+    if (chapter.campusPic?.url) {
+        //Add the background image to the style if the url exists in the db
+        heroStyle = {
+            backgroundImage: "url(" + chapter.campusPic.url + ")",
+        };
+    } else {
+        //Use placeholder gradient if no background image url exists in the db
+        heroStyle = {
+            backgroundImage: "linear-gradient(90deg, rgba(234,224,241,1) 0%, rgba(181,156,204,1) 100%)",
+        };
     }
-  }
-  else{ //Use placeholder gradient if no background image url exists in the db
-    heroStyle = {
-      backgroundImage: 'linear-gradient(90deg, rgba(234,224,241,1) 0%, rgba(181,156,204,1) 100%)'
-    }
-  }
- 
-  return(
-    <div>
-      
-      <Head>
-        <title>{cleanName} Chapter | MindVersity | A peer mental health network.</title>
-        <link rel="icon" href="../favicon.ico" />
-      </Head>
-      
-      <Header />
 
-      <div className="hero" style={heroStyle}>
-        <div className="heroOverlay">
-          <div className="heroText">
-            <h1>{cleanName}</h1>
-            <h2><FaMapMarkerAlt className="locationIcon" /> {chapter.city}, {chapter.state}</h2>
-          </div>
+    return (
+        <div>
+            <Head>
+                <title>{cleanName} Chapter | MindVersity | A peer mental health network.</title>
+                <link rel="icon" href="../favicon.ico" />
+            </Head>
+
+            <Header />
+
+            <div className="hero" style={heroStyle}>
+                <div className="heroOverlay">
+                    <div className="heroText">
+                        <h1>{cleanName}</h1>
+                        <h2>
+                            <FaMapMarkerAlt className="locationIcon" /> {chapter.city}, {chapter.state}
+                        </h2>
+                    </div>
+                </div>
+            </div>
+
+            <div className="bodyContent">
+                <div className="bodySection">
+                    <div className="topContentBox">
+                        <div className="aboutTextParent topContentBox-child">
+                            <h2>About</h2>
+                            <p className="aboutText">{chapter.description}</p>
+                        </div>
+                        {resources.length > 0 && (
+                            <div className="topContentBox-child">
+                                <h2>Chapter Specific Resources</h2>
+                                {resources.map((reso, i) => {
+                                    return (
+                                        <p className="resourceTextStyle" key={i}>
+                                            <a className="resourceLinkStyle" href={reso.link}>
+                                                {reso.name}
+                                            </a>
+                                        </p>
+                                    );
+                                })}
+                            </div>
+                        )}
+                    </div>
+                </div>
+
+                {officers.length > 0 && (
+                    <div className="chapterOfficerParent bodySection">
+                        <h2>Meet the Team</h2>
+                        <OfficerCarouselComp officers={officers} />
+                    </div>
+                )}
+            </div>
+
+            <Footer />
+
+            <style jsx>{`
+                .hero {
+                    width: 100%;
+                    height: 50vh;
+                    position: relative;
+                    background-image: url("journal-hero.jpeg");
+                    background-size: cover;
+                    background-position: center;
+                }
+
+                .heroOverlay {
+                    width: 100%;
+                    height: 100%;
+                    position: absolute;
+                    top: 0;
+                    bottom: 0;
+                    left: 0;
+                    right: 0;
+                    z-index: 2;
+                    background-color: rgba(215, 215, 215, 0.7);
+                }
+
+                .heroText {
+                    position: relative;
+                    display: block;
+                    top: 50%;
+                    transform: translateY(-50%);
+                    padding: 0px 40px;
+                    text-align: center;
+                }
+
+                h1 {
+                    position: relative;
+                    display: block;
+                    color: #503e8c;
+                    font-size: 46px;
+                    font-weight: bold;
+                    padding-bottom: 0px;
+                    margin-bottom: 0px;
+                }
+
+                .heroText h2 {
+                    font-weight: normal;
+                    margin-top: 0;
+                    padding-top: 20px;
+                    padding-left: 40px;
+                }
+
+                .locationIcon {
+                    color: #707070;
+                }
+
+                .bodyContent {
+                    width: auto;
+                    height: auto;
+                    position: relative;
+                    display: block;
+                    padding: 60px;
+                }
+
+                .bodySection {
+                    width: auto;
+                    height: auto;
+                    position: relative;
+                    display: block;
+                    padding: 20px 0px;
+                }
+
+                .aboutTextParent h2 {
+                    position: relative;
+                    display: block;
+                    text-align: left;
+                    font-size: 38px;
+                    color: #707070;
+                }
+
+                @media screen and (min-width: 770px) {
+                    .aboutText {
+                        font-size: 20px;
+                        color: #707070;
+                        width: 80%;
+                    }
+                }
+
+                @media screen and (max-width: 770px) {
+                    .aboutText {
+                        font-size: 20px;
+                        color: #707070;
+                        width: 100%;
+                    }
+                }
+
+                .chapterOfficerParent h2 {
+                    position: relative;
+                    display: block;
+                    text-align: left;
+                    font-size: 38px;
+                    color: black;
+                }
+
+                .topContentBox {
+                    display: flex;
+                }
+
+                .topContentBox-child {
+                    flex: 1;
+                }
+
+                .resourceTextStyle {
+                    margin-top: 30px;
+                    font-size: 24px;
+                    font-weight: 300;
+                }
+
+                .resourceLinkStyle {
+                    text-decoration: none;
+                    color: #000000;
+                }
+            `}</style>
+
+            <style jsx global>{`
+                html,
+                body {
+                    padding: 0;
+                    margin: 0;
+                    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell,
+                        Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+                }
+
+                * {
+                    box-sizing: border-box;
+                }
+            `}</style>
         </div>
-      </div>
-     
-      <div className="bodyContent">
-        <div className="aboutTextParent bodySection">
-          <h2>About</h2>
-          <p className="aboutText">
-            {chapter.description}
-          </p>
-        </div>
-        { officers.length > 0 &&
-        <div className="chapterOfficerParent bodySection">
-          <h2>Meet the Team</h2>
-          <OfficerCarouselComp officers={officers} />
-        </div>
-        }
-      </div>
-      
-      <Footer />
-
-      <style jsx>{`
-      .hero{
-        width: 100%;
-        height: 50vh;
-        position: relative;
-        background-image: url('journal-hero.jpeg');
-        background-size: cover;
-        background-position: center;
-      }
-
-      .heroOverlay{
-        width: 100%;
-        height: 100%;
-        position: absolute;
-        top: 0;
-        bottom: 0;
-        left: 0;
-        right: 0;
-        z-index: 2;
-        background-color: rgba(215, 215, 215, 0.70);
-      }
-
-      .heroText{
-        position: relative;
-        display: block;
-        top: 50%;
-        transform: translateY(-50%);
-        padding: 0px 40px;
-        text-align: center;
-      }
-
-      h1{
-        position: relative;
-        display: block;
-        color: #503E8C;
-        font-size: 46px;
-        font-weight: bold;
-        padding-bottom: 0px;
-        margin-bottom: 0px;
-      }
-
-      .heroText h2{
-        font-weight: normal;
-        margin-top: 0;
-        padding-top: 20px;
-        padding-left: 40px
-      }
-
-      .locationIcon{
-        color: #707070;
-      }
-
-      .bodyContent{
-        width: auto;
-        height: auto;
-        position: relative;
-        display: block;
-        padding: 60px;
-      }
-
-      .bodySection{
-        width: auto;
-        height: auto;
-        position: relative;
-        display: block;
-        padding: 20px 0px;
-      }
-
-      .aboutTextParent h2{
-        position: relative;
-        display: block;
-        text-align: left;
-        font-size: 38px;
-        color: #707070;
-      }
-
-      @media screen and (min-width: 770px){
-        .aboutText{
-          font-size: 20px;
-          color: #707070;
-          width: 40%;
-        }  
-      }
-
-      @media screen and (max-width: 770px){
-        .aboutText{
-          font-size: 20px;
-          color: #707070;
-          width: 100%;
-        }  
-      }
-      
-      .chapterOfficerParent h2{
-        position: relative;
-        display: block;
-        text-align: left;
-        font-size: 38px;
-        color: black;
-      }
-
-	  `}</style>
-
-	  <style jsx global>{`
-      html,
-      body {
-      padding: 0;
-      margin: 0;
-      font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-        Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-        sans-serif;
-      }
-
-      * {
-      box-sizing: border-box;
-      }
-  	`}</style>
-    </div>
-  );
+    );
 };
 
-// This function cant be in child component, so we query the data and 
+// This function cant be in child component, so we query the data and
 // then pass it to the component. It's ran server-side
 export async function getStaticProps(context: GetStaticPropsContext) {
-  // query by the chapter's name
-  let chapterQuery: Chapter = new Object;
-  chapterQuery.name = context.params?.name as string;
+    // query by the chapter's name
+    const chapterQuery: Chapter = new Object();
+    chapterQuery.name = context.params?.name as string;
 
-  var chapter: Chapter = new Object;
-  var chapters: Chapter[] = await getChapters(chapterQuery);
-  if (chapters.length === 1) {
-    chapter = chapters[0];
-  }
-  else {
-    // TODO route to an error page
-    throw new Error(errors.GENERIC_ERROR);
-  }
+    let chapter: Chapter = new Object();
+    const chapters: Chapter[] = await getChapters(chapterQuery);
+    if (chapters.length === 1) {
+        chapter = chapters[0];
+    } else {
+        // TODO route to an error page
+        throw new Error(errors.GENERIC_ERROR);
+    }
 
-  let officerQuery: Officer = new Object;
-  officerQuery.chapter = chapter.name;
+    const officerQuery: Officer = new Object();
+    officerQuery.chapter = chapter.name;
 
-  var officers: Officer[] = await getOfficers(officerQuery);
-  if (!(officers.length > 0))
-  {
-    // TODO route to an error page
-    //throw new Error(errors.GENERIC_ERROR);
-  }
+    const officers: Officer[] = (await getOfficers(officerQuery)) as Officer[];
+    if (!(officers.length > 0)) {
+        // TODO route to an error page
+        //throw new Error(errors.GENERIC_ERROR);
+    }
 
-  return {
-    props: {
-      chapter: JSON.parse(JSON.stringify(chapter)),
-      officers: JSON.parse(JSON.stringify(officers)),
-    },
-  }
+    const resources = await getResources({ chapter: chapterQuery.name });
+
+    return {
+        props: {
+            chapter: JSON.parse(JSON.stringify(chapter)) as Chapter,
+            officers: JSON.parse(JSON.stringify(officers)) as Officer[],
+            resources: JSON.parse(JSON.stringify(resources)) as Resource[],
+        },
+    };
 }
 
 export async function getStaticPaths() {
-  let chapters: Chapter[] = await getChapters({});
+    const chapters: Chapter[] = await getChapters({});
 
-  let paths = chapters.map((chapter) => ({
-    params: {name: chapter.name},
-  }));
+    const paths = chapters.map(chapter => ({
+        params: { name: chapter.name },
+    }));
 
-  return { paths, fallback: false}
+    return { paths, fallback: false };
 }
 
 export default ChapterPage;
